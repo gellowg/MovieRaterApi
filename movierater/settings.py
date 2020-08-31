c909c62914c9d5c4840cb748315e3eb7e827a114
george <george@blius.net>
2020-08-31 19:26:34 +0100

    Added domain to CORS

--- movierater/settings.py
+++ movierater/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000", "http://127.0.0.1:3000", "https://movierater.georgetong.uk/"
+    "http://localhost:3000", "http://127.0.0.1:3000", "https://movierater.georgetong.uk"
 ]
 
 ROOT_URLCONF = 'movierater.urls'
