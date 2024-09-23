3dbaa5ead136cfdc68a8ca0f9d7eaabc91c67a51
gellowg <47644702+gellowg@users.noreply.github.com>
2024-09-23 12:02:35 +0100

    Update settings.py
    added new allowed hosts

--- movierater/settings.py
+++ movierater/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['movie-rater-geonitic.herokuapp.com']
+ALLOWED_HOSTS = ['movie-rater-geonitic.herokuapp.com', 'localhost', 'movierater.georgetong.cloud']
 
 
 # Application definition
